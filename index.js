e5f971bbf63219ba2fd588cda7fe20b23015e937
AthraaMosawi <athraamosawi169@gmail.com>
2024-02-12 00:00:18 +0300

    solve the task

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function showYoungestStudent(students) { }
+function showYoungestStudent(students) {
+    if (students.length === 0) {
+        console.log('No students provided.');
+        return;
+    }
+
+    const youngestStudent = students.reduce((youngest, student) => {
+        return student.age < youngest.age ? student : youngest;
+    }, students[0]);
+
+    console.log(youngestStudent.name);
+}
+
+module.exports = showYoungestStudent;
+
 
 module.exports = showYoungestStudent;
